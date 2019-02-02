a6ba4d6b9044aa82f3d4bac95d07ac1bfd4f59a3
johnf007 <johnf@qti.qualcomm.com>
2019-02-02 01:15:10 +0000

    my changes

--- keras-cnn/cnn.py
+++ keras-cnn/cnn.py
@@ -38,10 +38,17 @@ model = Sequential()
 model.add(Conv2D(32,
     (config.first_layer_conv_width, config.first_layer_conv_height),
     input_shape=(28, 28,1),
-    activation='relu'))
+    activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.4))
+model.add(Conv2D(64,
+    (config.first_layer_conv_width, config.first_layer_conv_height),
+    activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.4))
 model.add(Flatten())
 model.add(Dense(config.dense_layer_size, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam',
--- keras-perceptron/perceptron-logistic.py
+++ keras-perceptron/perceptron-logistic.py
@@ -25,6 +25,7 @@ num_classes = y_train.shape[1]
 # create model
 model = Sequential()
 model.add(Flatten(input_shape=(img_width, img_height)))
+#Generally for classification you want to use softmax
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam',
               metrics=['accuracy'])
